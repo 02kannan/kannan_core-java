dc81aa3914b73b0c9d3e3d5e782d016ecaa00ef9
kannan <kannan@DESKTOP-ESRTOT3>
2024-08-02 21:34:07 +0530

    modifiers

---
+++ Javadevlop/src/Differentmodifiers/Sample.java
@@ -0,0 +1,21 @@
+package Differentmodifiers;
+
+import modifiers.AccessModifier;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+				AccessModifier s= new AccessModifier();
+				s.a();
+				s.b();
+				s.c();
+				s.d();
+
+			}
+
+		
+
+	}
+
+
